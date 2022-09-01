edb9730431a3b701fc16a24d3b8a8917e7a5c02f
Detrivinog <detrivinog@gmail.com>
2022-09-01 08:33:59 -0500

    Agregar funciones y maquetado final

--- src/components/Item.js
+++ src/components/Item.js
@@ -9,11 +9,23 @@
 //    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
 //    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
 
-export default function Item() {
+import { useState } from "react"
+
+export default function Item({addItem, item}) {
+
+  const [stock, setStock] = useState(item.stock)
+  const onClickHandler = () => {
+    addItem()
+    setStock(stock-1)
+  }
 
   return (
     <div className='producto'>
       {/* maquetar Item aquí */}
+      <h3>{item.producto.nombre}</h3>
+      <p>{item.producto.descripcion}</p>
+      <h5>En stock: <span>{stock > 0 ? stock : 'agotado'}</span></h5>
+      <button onClick={() => onClickHandler()} disabled = {stock<=0}>{stock > 0 ? 'COMPRAR' : 'SIN STOCK'}</button>
     </div>
   )
 }
